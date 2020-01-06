9c1222bd59991f67ab8516335f56233df1846a54
Yashadatiya <47525332+Yashadatiya@users.noreply.github.com>
2020-01-06 17:03:37 +0530

    public remove

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <!-- <title>{{config('constants.SITE_NAME')}} | @yield('title')</title> -->
     <title>Yokart | @yield('title')</title>
     <!-- Scripts -->
-    <!-- <script src="{{ asset('public/public/js/app.js') }}" defer></script> -->
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,21 +19,21 @@
     <!-- Font-Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <link href="{{ asset('public/front/css-thirdparty/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('front/css-thirdparty/bootstrap.min.css')}}" rel="stylesheet">
     <!-- <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/> -->
-    <link href="{{ asset('public/front/css-thirdparty/owl.carousel.css')}}" rel="stylesheet">
+    <link href="{{ asset('front/css-thirdparty/owl.carousel.css')}}" rel="stylesheet">
     <!-- Main CSS -->
-    <link rel="stylesheet" href="{{ asset('public/front/css/main.css')}}">
-    <!-- <link rel="stylesheet" href="{{ asset('public/front/style.css')}}"> -->
-    <script src="{{ asset('public/front/js/thirdparty/jquery-3.2.1.min.js')}}"></script>
+    <link rel="stylesheet" href="{{ asset('front/css/main.css')}}">
+    <!-- <link rel="stylesheet" href="{{ asset('front/style.css')}}"> -->
+    <script src="{{ asset('front/js/thirdparty/jquery-3.2.1.min.js')}}"></script>
     <!-- <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> -->
-    <script type="text/javascript" src="{{ asset('public/front/js/thirdparty/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('front/js/thirdparty/bootstrap.min.js')}}"></script>
     <link href="https://cdn.jsdelivr.net/jquery.slick/1.4.1/slick.css" rel="stylesheet">
     <link rel='stylesheet' href='https://cdn.rawgit.com/StarPlugins/thumbelina/8b9c09d9/thumbelina.css'>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.slick/1.4.1/slick.min.js"></script>
-    <script type="text/javascript" src="{{ asset('public/front/js/thirdparty/owl.carousel.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('front/js/thirdparty/owl.carousel.min.js')}}"></script>
     <script src='https://cdn.rawgit.com/StarPlugins/thumbelina/8b9c09d9/thumbelina.js'></script>
-    <script  src="{{ asset('public/front/js/zoom.js') }}"></script>
+    <script  src="{{ asset('front/js/zoom.js') }}"></script>
 
     <style type="text/css">
       input[type=number]::-webkit-inner-spin-button,
@@ -78,7 +78,7 @@
        {{--  <div class="main-header">
             <div class="logo-header" data-background-color="blue">
                 <a href="" class="logo">
-                    <img src="{{ asset('public/public/images/logo-white.png')}}" alt="navbar brand" class="navbar-brand" style="height:60%">
+                    <img src="{{ asset('images/logo-white.png')}}" alt="navbar brand" class="navbar-brand" style="height:60%">
                 </a>
                 <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon">
@@ -113,7 +113,7 @@
         $(".wholesale-box").slideUp();
       });
     </script>
-    <script type="text/javascript" src="{{ asset('public/front/js/app.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('front/js/app.min.js')}}"></script>
 
 </body>
 </html>
